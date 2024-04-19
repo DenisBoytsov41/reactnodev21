38400d3af2c35563074139e611fd73b03aba3ed8
lab1_web <goodpromised41@gmail.com>
2024-04-19 18:59:53 +0300

    fix

--- back/server.js
+++ back/server.js
@@ -13,6 +13,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
+  //sfs
 });
 
 app.post('/register', async (req, res) => {
